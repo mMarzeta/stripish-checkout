f025aca40e797859106cf749006aed828c0a76d0
Maciej M <94maciekm@gmail.com>
2024-02-01 19:41:29 +0100

    SASS, vite, vue, bootstrap config

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Basket from './components/Basket.vue'
+import Checkout from './components/Checkout.vue'
+</script>
+
+<template>
+  <div class="container-fluid">
+    <div class="row align-items-center">
+      <div class="col col-main-dark">
+        <Basket/>
+      </div>
+      <div class="col col-main-light">
+        <Checkout/>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.container-fluid {
+  background-color: #592d03;
+  border: 1px solid red;
+  height: 100vh;
+}
+</style>
---
+++ src/components/Basket.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  Item 1
+  Item 2
+</template>
+
+<style scoped>
+
+</style>

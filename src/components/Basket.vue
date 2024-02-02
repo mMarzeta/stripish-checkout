44a258655078f03053d003e042f5c09757962ed2
Maciej M <94maciekm@gmail.com>
2024-02-02 18:25:24 +0100

    Logo and back text, without proper animation

--- src/components/Basket.vue
+++ src/components/Basket.vue
@@ -1,12 +1,30 @@
 <script setup>
+import BackButton from './buttons/BackButton.vue'
 
 </script>
 
 <template>
-  Item 1
-  Item 2
+  <div class="container">
+    <div class="back-button">
+      <BackButton/>
+    </div>
+    <br>
+    Pay ShipFast
+    <br>
+    <h1>
+      US$199.00
+    </h1>
+  </div>
 </template>
 
 <style scoped>
+.container {
+  margin-top: 40px;
+  margin-left: 60px;
+}
 
+.back-button {
+  margin-bottom: 20px;
+  margin-left: -20px;
+}
 </style>
---
+++ src/components/buttons/BackButton.vue
@@ -0,0 +1,75 @@
+<script setup>
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faArrowLeft} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import {ref} from 'vue'
+
+library.add(faArrowLeft)
+
+const showLogo = ref(false)
+
+const handleMouseOver = () => {
+  showLogo.value = true
+}
+
+const handleMouseOut = () => {
+  showLogo.value = false
+}
+
+</script>
+
+<template>
+  <a href="/" @mouseover="handleMouseOver" @mouseout="handleMouseOut">
+    <FontAwesomeIcon class="backIcon" :class="{ active: showLogo}" :icon="['fas', 'arrow-left']"/>
+    <transition name="fade" mode="out-in">
+      <span v-if="showLogo" class="backText">
+        Back
+      </span>
+      <span v-else class="backText">
+        <img src="@/components/icons/sample-logo.png" alt="Logo" class="backLogo">
+        Logo
+      </span>
+    </transition>
+  </a>
+  <br>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/_variables.scss';
+
+a {
+  text-decoration: none;
+  color: $button-back-light;
+}
+
+a:hover {
+  color: $button-back-hover;
+}
+
+.backIcon {
+  margin-right: 10px;
+}
+
+.fade-enter-active {
+  transition: opacity 0.15s;
+}
+
+.fade-leave-active {
+  transition: opacity 0s;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+
+.active {
+  margin-left: -10px;
+}
+
+.backLogo {
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+</style>
